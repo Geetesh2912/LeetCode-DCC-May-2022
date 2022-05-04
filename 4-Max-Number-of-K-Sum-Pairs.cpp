a9f9a6cffc8b9ef50f4b40c1bff0ff8f4894a65c
Geetesh Mandaogade <57803929+Geetesh2912@users.noreply.github.com>
2022-05-05 00:27:32 +0530

    Update 4-Max-Number-of-K-Sum-Pairs.cpp

--- 4-Max-Number-of-K-Sum-Pairs.cpp
+++ 4-Max-Number-of-K-Sum-Pairs.cpp
@@ -1,1 +1,22 @@
+//author: Geetesh Mandaogade
 
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        //this problem is very similar to two sum
+        int count=0;
+        unordered_map<int,int> myMap;
+        
+        for (int i=0; i<nums.size(); i++)
+        {
+            if (myMap[k-nums[i]]!=0)
+            {
+                count++;
+                myMap[k-nums[i]]--;
+            }
+            else
+                myMap[nums[i]]++;
+        }
+        return count;
+    }
+};
